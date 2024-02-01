afcc20df6bf59bad64f258aabc7ca4d4082d67fe
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-01 06:38:47 -0300

    atualizando array de portas

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
-'use client'
+"use client";
 import Porta from "@/components/Porta";
+import { atualizarPorta, criarPortas } from "@/functions/portas";
 import PortaModel from "@/model/porta";
 import { useState } from "react";
 
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1, false, true));
+  const [portas, setPortas] = useState(criarPortas(3, 2));
 
-  return (
-    <div style={{ display: "flex" }}>
-      <Porta value={p1} onChange={setP1}/>
-    </div>
-  );
+  const atualizarPortas = (novaPorta: PortaModel) => {
+    setPortas(atualizarPorta(portas, novaPorta));
+  };
+
+  function renderizarPortas() {
+    return portas.map((porta) => {
+      return (
+        <Porta key={porta.numero} value={porta} onChange={atualizarPortas} />
+      );
+    });
+  }
+
+  return <div style={{ display: "flex" }}>{renderizarPortas()}</div>;
 }
--- functions/portas.ts
+++ functions/portas.ts
@@ -1,9 +1,26 @@
 import PortaModel from "@/model/porta";
 
-export function criarPortas(quantidade: number, selecionada: number): PortaModel[] {
-    return Array.from({length: quantidade}, (_, i) => {
-        const numero = i  + 1
-        const temPresente = numero === selecionada
-        return new PortaModel(numero, temPresente)
-    })
+export function criarPortas(
+  quantidade: number,
+  selecionada: number
+): PortaModel[] {
+  return Array.from({ length: quantidade }, (_, i) => {
+    const numero = i + 1;
+    const temPresente = numero === selecionada;
+    return new PortaModel(numero, temPresente);
+  });
+}
+
+export function atualizarPorta(
+  portas: PortaModel[],
+  portaModificada: PortaModel
+): PortaModel[] {
+  return portas.map((portaAtual) => {
+    const igualAModificada = portaAtual.numero === portaModificada.numero;
+
+    if (igualAModificada) 
+        return portaModificada;
+
+    return portaModificada.aberta ? portaAtual : portaAtual.desselecionar();
+  });
 }
