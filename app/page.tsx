d4a8fc22b7d8d4cbfa6ebe2c3bc4a6f949415e6b
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-02 18:19:38 -0300

    implementando componente EntradaNumerica

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
+'use client'
 import styles from "@/styles/Formulario.module.css";
 import Cartao from "@/components/Cartao";
 import Link from "next/link";
+import EntradaNumerica from "@/components/EntradaNumerica";
+import { useState } from "react";
 
 export default function Fomrulario() {
+  const [quantidadePortas, setQuantidadePortas] = useState(3);
+
   return (
     <div className={styles.formulario}>
       <div>
         <Cartao bgcolor="#c0392c">
           <h1>Monty Hall</h1>
         </Cartao>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica text="Quantidade Portas?" value={quantidadePortas} onChange={setQuantidadePortas} />
+        </Cartao>
       </div>
       <div>
         <Cartao></Cartao>
